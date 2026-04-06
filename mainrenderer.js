8930fdd279ad696e3c5a8087b4c416a23e1d03e2
yjchoi-curiosis <yjchoi@curiosis.com>
2026-04-06 16:49:23 +0900

    version cache error debug

--- mainrenderer.js
+++ mainrenderer.js
@@ -1,23 +1,26 @@
- import { items } from './data/items.js';
+const VERSION = "20260406";
 
 async function render() {
   const list = document.getElementById('list');
   list.innerHTML = '';
 
+  const res = await fetch(`./data/items.json?v=${VERSION}`);
+  const items = await res.json();
+
   for (let i = 0; i < items.length; i++) {
     const item = items[i];
     list.innerHTML += `
-        <tr>
-            <td>${i + 1}</td>
-            <td>${item.name}</td>
-            <td>${item.desc}</td>
-            <td>
-            <a href="https://search.naver.com/search.naver?query=${encodeURIComponent(item.name)}" target="_blank">
-                검색
-            </a>
-            </td>
-        </tr>
-        `;
+      <tr>
+        <td>${i + 1}</td>
+        <td>${item.name}</td>
+        <td>${item.desc}</td>
+        <td>
+          <a href="https://search.naver.com/search.naver?query=${encodeURIComponent(item.name)}" target="_blank">
+            검색
+          </a>
+        </td>
+      </tr>
+    `;
   }
 }
 
