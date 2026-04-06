0ebd97520d62750511c3464aa9800a21bce42f32
yjchoi-curiosis <yjchoi@curiosis.com>
2026-04-06 17:19:36 +0900

    ui design update

--- mainrenderer.js
+++ mainrenderer.js
@@ -1,28 +1,20 @@
-const VERSION = "20260407";
-fetch(`./data/items.json?v=${VERSION}`);
+const res = await fetch(`./data/items.json?v=${Date.now()}`);
+const items = await res.json();
 
-async function render() {
-  const list = document.getElementById('list');
-  list.innerHTML = '';
+const list = document.getElementById('list');
+list.innerHTML = '';
 
-  const res = await fetch(`./data/items.json?v=${VERSION}`);
-  const items = await res.json();
-
-  for (let i = 0; i < items.length; i++) {
-    const item = items[i];
-    list.innerHTML += `
-      <tr>
-        <td>${i + 1}</td>
-        <td>${item.name}</td>
-        <td>${item.desc}</td>
-        <td>
-          <a href="https://search.naver.com/search.naver?query=${encodeURIComponent(item.name)}" target="_blank">
-            검색
-          </a>
-        </td>
-      </tr>
-    `;
-  }
-}
-
-render();
+items.forEach((item, i) => {
+  list.innerHTML += `
+    <tr>
+      <td>${i + 1}</td>
+      <td><strong>${item.name}</strong></td>
+      <td class="desc">${item.desc}</td>
+      <td>
+        <a class="btn" href="https://search.naver.com/search.naver?query=${encodeURIComponent(item.name)}" target="_blank">
+          검색
+        </a>
+      </td>
+    </tr>
+  `;
+});
